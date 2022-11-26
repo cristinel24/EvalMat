eae0eafe5834019983cd7d2b9c40d6e0ef81d5f4
Andrei Cristian <cristianandrei987@gmail.com>
2022-11-26 13:48:22 +0200

    comentarii

--- Conversii.h
+++ Conversii.h
@@ -2,17 +2,17 @@
 #include <string>
 char operanzi[] = "+-/*()%";
 
-bool operand(string x) {
+bool operand(string x) { ///returneaza 1 daca se gaseste in lista de operanzi, 0 altfel
 	return strchr(operanzi,x[0]);
 }
 
 void convInfix2Postfix(queue<string>& infix) {
 	stack<string> S;
-	init();
+	init(); /// Initiez prioritatile operanzilor
 	string x;
 	while (!infix.empty()) {
 		x = infix.front(); infix.pop();
-		if (!operand(x))
+		if (!operand(x)) ///Verific daca nu este operand
 			postfix.push(x);
 		else {
 			if (x == ")") {
@@ -36,18 +36,18 @@ void convInfix2Postfix(queue<string>& infix) {
 }
 
 double valpostfix(queue<string>& postfix) {
-	convInfix2Postfix(infix);
-	stack<string> S;
+	convInfix2Postfix(infix); //generez notarea postfixata in coada "postifx"
+	stack<string> S; //Stiva S
 	string x, st, dr;
 	double val, valst,valdr;
 	while (!postfix.empty()) {
 		x = postfix.front(); postfix.pop();
-		if (!operand(x))
+		if (!operand(x)) ///Verific daca nu este operand
 			S.push(x);
 		else {
 			dr = S.top(); S.pop();
 			st = S.top(); S.pop();
-			valdr = stod(dr);
+			valdr = stod(dr); //transforma din string in double
 			valst = stod(st);
 			switch (x[0]) {
 				case '+': val = valst + valdr; break;
@@ -58,8 +58,8 @@ double valpostfix(queue<string>& postfix) {
 					val = valst / valdr; break;
 				case '%': val = int(valst) % int(valdr); break;
 			}
-			string val_s = to_string(val);
-			S.push(val_s);
+			string val_s = to_string(val); //transform din double in string
+			S.push(val_s); //pun string in stiva
 		}
 	}
 	x = S.top(); S.pop();
